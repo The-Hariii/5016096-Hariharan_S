5bc297fe32eb91ca7a5e3fd2f3fef7c2c46455ef
Hariharan S <121422818+The-Hariii@users.noreply.github.com>
2024-08-28 17:06:24 +0530

    Delete WEEK 4 AND 5/BOOKSTORE/BookStore directory

--- WEEK 4 AND 5/BOOKSTORE/BookStore/src/main/java/com/example/BookStore/Repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.BookStore.Repository;
-
-import com.example.BookStore.Entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
